ec7b5bc1cefe40511666a64f8330fdcb31d3a313
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-08-07 19:19:31 +1200

    DS_Store files, hunf

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dork scripts
+
+You probably don't need these scripts.
+
+But in case you do, all licensed under CC 4.0 International.
---
+++ wutsinside-dsstore.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# What is inside .DS_Store?
+# Found a .DS_Store in one of the servers from $work, but not a Mac user... so what's
+# inside that file anyway?
+
+import sys
+import argparse
+from ds_store import DSStore
+
+parser = argparse.ArgumentParser(description='See inside a .DS_Store file')
+parser.add_argument('--file', help='File absolute location', required=True)
+
+import logging
+from pprint import pprint
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
+def main():
+    args       = parser.parse_args()
+    file_in    = args.file
+
+    logging.info("Opening .DS_Store file %s" % file_in)
+
+    with DSStore.open(file_in) as dsfile:
+        for elem in dsfile:
+            logging.info("- File [%s] %s bytes" % (elem.filename, elem.byte_length()))
+            print(elem.type)
+
+    logging.info("All done!")
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
