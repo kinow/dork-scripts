e373a1770c225c2add7dee1737047f4efd2cff03
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-08-26 16:28:57 +1200

    Typos

--- databases/mysql2sqlite.sh
+++ databases/mysql2sqlite.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Converts a mysqldump file into a Sqlite 3 compatible file. It also extracts the MySQL `KEY xxxxx` from the
-# CREATE block and create them in separate commands _after_ all the INSERTs.
+# CREATE block and create them in separate commands _after_ all the INSERT's.
 
-# Awk is choosen because it's fast and portable. You can use gawk, original awk or even the lightning fast mawk.
+# Awk is chosen because it's fast and portable. You can use gawk, original awk or even the lightning fast mawk.
 # The mysqldump file is traversed only once.
 
 # Usage: $ ./mysql2sqlite mysqldump-opts db-name | sqlite3 database.sqlite
