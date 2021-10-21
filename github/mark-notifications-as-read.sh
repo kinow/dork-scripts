268080f8d38238c3ae81a6fa94575f352ecad7f6
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2021-10-22 12:09:46 +1300

    Update mark-notifications-as-read.sh

--- github/mark-notifications-as-read.sh
+++ github/mark-notifications-as-read.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# https://docs.github.com/en/rest/reference/activity#mark-notifications-as-read
+
 curl -X PUT \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token blabla" \
-  https://api.github.com/notifications -d "{'last_read_at': '$(date '+%Y-%m-%dT%H:%M:%S')'}"
+  https://api.github.com/notifications -d '{"last_read_at": "'$(date '+%Y-%m-%dT%H:%M:%SZ')'"}'
