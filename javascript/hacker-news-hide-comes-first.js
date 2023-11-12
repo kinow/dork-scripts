86d09a6cde54fce917eaececee4e5bf462c0da91
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2023-11-12 13:33:40 +0100

    Update hacker-news-hide-comes-first.js

--- javascript/hacker-news-hide-comes-first.js
+++ javascript/hacker-news-hide-comes-first.js
@@ -1,9 +1,7 @@
 // Moves the hide button to the first position of child elements, so that you can click to hide entries without moving your mouse
-for (const subtext of $$('.subtext')) {
-  const children = [...subtext.children]
-  const hide = children.find(child => child.text === 'hide')
-  if (hide) {
-    subtext.removeChild(hide)
-    subtext.prepend(hide)
-  }
+for (const comment of $$('.comhead')) {
+  const navs = comment.querySelector('.navs')
+  const hide = navs.querySelector('.togg')
+  navs.removeChild(hide)
+  comment.prepend(hide)
 }
