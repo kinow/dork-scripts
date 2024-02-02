c86c5cfb3471234a4c3cc461713a7103e1431934
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2024-02-02 23:55:27 +0100

    Convert avif too

--- thunar/webp-to-png.sh
+++ thunar/webp-to-png.sh
@@ -2,9 +2,9 @@
 #
 # 1. Open Thunar
 # 2. Select "Edit" / "Configure custom actions..."
-# 3. Enter the name "WebP to PNG"
-# 4. Description "Convert WebP images to PNG and delete the WebP image"
+# 3. Enter the name "WebP/Avif to PNG"
+# 4. Description "Convert WebP and Avif images to PNG and delete the WebP image"
 # 5. Set the command to the example below
 # 6. Grab an icon for the context menu shortcut, e.g. https://www.flaticon.com
 
-find . -maxdepth 1 -name "*.webp" -exec bash -c 'convert "{}" "$(basename "{}" | rev | cut -d. -f2- | rev).png" && rm "{}"' \;
+find . -maxdepth 1 -regex ".*\(\,webp\|\.avif\)" -exec bash -c 'convert "{}" "$(basename "{}" | rev | cut -d. -f2- | rev).png" && rm "{}"' \;
