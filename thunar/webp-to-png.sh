b4a5d6cdc9564f06bf9c23d092645fb9ad848dc6
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2024-02-10 23:10:47 +0100

    Correct the extension webp regex

--- thunar/webp-to-png.sh
+++ thunar/webp-to-png.sh
@@ -7,4 +7,4 @@
 # 5. Set the command to the example below
 # 6. Grab an icon for the context menu shortcut, e.g. https://www.flaticon.com
 
-find . -maxdepth 1 -regex ".*\(\,webp\|\.avif\)" -exec bash -c 'convert "{}" "$(basename "{}" | rev | cut -d. -f2- | rev).png" && rm "{}"' \;
+find . -maxdepth 1 -regex ".*\(\.webp\|\.avif\)" -exec bash -c 'convert "{}" "$(basename "{}" | rev | cut -d. -f2- | rev).png" && rm "{}"' \;
