3036ce5548a7b5846cd0e97ddf0f48de139ccd2f
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2024-04-27 01:31:52 +0200

    Update webp-to-png.sh

--- thunar/webp-to-png.sh
+++ thunar/webp-to-png.sh
@@ -1,5 +1,9 @@
 # Ref: https://docs.xfce.org/xfce/thunar/4.14/custom-actions
 #
+# Changelog:
+#
+# 2024-04-27: keep mtime when doing modifications
+#
 # 1. Open Thunar
 # 2. Select "Edit" / "Configure custom actions..."
 # 3. Enter the name "WebP/Avif to PNG"
@@ -7,4 +11,4 @@
 # 5. Set the command to the example below
 # 6. Grab an icon for the context menu shortcut, e.g. https://www.flaticon.com
 
-find . -maxdepth 1 -regex ".*\(\.webp\|\.avif\)" -exec bash -c 'convert "{}" "$(basename "{}" | rev | cut -d. -f2- | rev).png" && rm "{}"' \;
+find . -maxdepth 1 -regex ".*\(\.webp\|\.avif\)" -exec bash -c 'mtime=$(stat -c %y "{}"); target=$(basename "{}" | rev | cut -d. -f2- | rev).png; convert "{}" "${target}" && touch -d "$mtime" "${target}" && rm "{}"' \;
