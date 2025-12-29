5120459338f6e8cef336ea1c3d1026642cb30b03
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2025-12-29 12:54:27 +0100

    Update safer_npm_install.sh

--- javascript/safer_npm_install.sh
+++ javascript/safer_npm_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run --rm -it -v "$PWD:/app" -w /app node:20-alpine npm install --ignore-scripts
+docker run --user "$(id -u):$(id -g)" --rm -it -v "$PWD:/app" -w /app node:20-alpine npm install --ignore-scripts
+docker run --user "$(id -u):$(id -g)" --rm -it -v "$PWD:/app" -w /app node:20-alpine npm run build --ignore-scripts
