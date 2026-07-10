18663ff4175bf58987a4ad4518b9bbb6ecc50f89
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2026-07-11 00:15:11 +0200

    Create secure-yarn.sh

---
+++ javascript/secure-yarn.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# nvm install --lts
+# nvm use --lts
+# corepack enable
+# corepack prepare yarn@stable --activate
+yarn config set enableScripts false
+yarn config set enableImmutableInstalls true
